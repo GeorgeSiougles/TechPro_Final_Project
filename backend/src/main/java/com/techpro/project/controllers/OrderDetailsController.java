8cb857360c8806950a64869c0a1c0acb92d5dabc
George Siougles <jojossiougles@gmail.com>
2023-06-22 14:51:23 +0000

    Implement put mapping in orderDetails controller

--- backend/src/main/java/com/techpro/project/controllers/OrderDetailsController.java
+++ backend/src/main/java/com/techpro/project/controllers/OrderDetailsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techpro.project.entity.OrderDetails;
@@ -14,6 +15,8 @@ import com.techpro.project.repository.OrderDetailsRepository;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
+
+
 
 
 @CrossOrigin("*")
@@ -37,12 +40,21 @@ public class OrderDetailsController {
         return orderDetailsRepository.findById(id).orElseThrow(()-> new OrderDetailsNotFoundException(id));
     }
 
-    @DeleteMapping("orderDetails/{id}")
+    @DeleteMapping("/orderDetails/{id}")
     String deleteOrderDetails(@PathVariable Integer id){
         if(!orderDetailsRepository.existsById(id)){
             throw new OrderDetailsNotFoundException(id);
         }
         orderDetailsRepository.deleteById(id);
         return "Order Details with id " + id + " has been deleted successfully";
+    }
+
+    @PutMapping("/orderDetails/{id}")
+    OrderDetails updateOrderDetails(@RequestBody OrderDetails newOrderDetails,@PathVariable Integer id){
+        return orderDetailsRepository.findById(id).map( details -> {
+            details.setItem(newOrderDetails.getItem());
+            details.setQuantity(newOrderDetails.getQuantity());
+            return orderDetailsRepository.save(details);
+        }).orElseThrow(()-> new OrderDetailsNotFoundException(id));
     }
 }
