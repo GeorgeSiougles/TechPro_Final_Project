b09019af96b830ab1f73d4cc958d61b931b03a94
George Siougles <jojossiougles@gmail.com>
2023-06-22 18:09:21 +0000

    Add items and orders lists to homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,11 +4,15 @@ import axios from "axios";
 
 export default function Home() {
   const [people, setPeople] = useState([]);
+  const [items, setItems] = useState([]);
+  const [orders, setOrders] = useState([]);
 
   const { id } = useParams();
 
   useEffect(() => {
     loadPeople();
+    loadItems();
+    loadOrders();
   }, []);
 
   const loadPeople = async () => {
@@ -21,10 +25,30 @@ export default function Home() {
     loadPeople();
   };
 
+  const loadItems = async () => {
+    const result = await axios.get("http://localhost:8090/getAllItems");
+    setItems(result.data);
+  };
+
+  const deleteItem = async (id) => {
+    await axios.delete(`http://localhost:8090/item/${id}`);
+    loadItems();
+  };
+
+  const loadOrders = async () => {
+    const result = await axios.get("http://localhost:8090/getAllOrderDetails");
+    setOrders(result.data);
+  };
+
+  const deleteOrder = async (id) => {
+    await axios.delete(`http://localhost:8090/orderDetails/${id}`);
+    loadOrders();
+  };
+
   return (
     <div className="container">
       <div className="py-4">
-        <table className="table border shadow">
+        <table className="table border shadow my-2">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -59,6 +83,88 @@ export default function Home() {
                   <button
                     className="btn btn-danger mx-2"
                     onClick={() => deletePerson(person.personId)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <table className="table border shadow my-2">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Item Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{item.name}</td>
+                <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                    to={`/viewItem/${item.itemId}`}
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/editItem/${item.itemId}`}
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => deleteItem(item.itemId)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <table className="table border shadow my-2">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Order Date</th>
+              <th scope="col">Item</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Contact Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{order.orderTable.orderDate}</td>
+                <td>{order.item.name}</td>
+                <td>{order.quantity}</td>
+                <td>{order.orderTable.person.email}</td>
+                <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                    to={`/viewOrder/${order.id}`}
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/editOrder/${order.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => deleteOrder(order.id)}
                   >
                     Delete
                   </button>
